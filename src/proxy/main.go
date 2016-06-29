bf3e31df1ffd56525eb8c380056c09becc330b05
Klaus Jrgensen <klaus@blackwoodseven.com>
2016-06-29 21:54:59 +0200

    add loging

--- src/proxy/main.go
+++ src/proxy/main.go
@@ -7,7 +7,16 @@ import (
     "net/http"
     "net/http/httputil"
     "strings"
+    "time"
 )
+
+func Log(handler http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        before := time.Now()
+        handler.ServeHTTP(w, r)
+        log.Printf("%s %s %.4f %s\n", r.RemoteAddr, r.Method, time.Since(before), r.URL)
+    })
+}
 
 func main() {
     port := flag.Int("port", 80, "listening on port")
@@ -16,7 +25,6 @@ func main() {
     fmt.Println("starting proxy on port", *port)
     log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), &httputil.ReverseProxy{
         Director: func(req *http.Request) {
-            log.Print(req.Host)
             req.URL.Host = strings.Split(req.Host, ".")[0]
             req.URL.Scheme = "http"
         },
