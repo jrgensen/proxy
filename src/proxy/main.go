d2d09a7f09aa3cf2069d04b640bcdeaee27c86b1
Christian Schmidt <cs@blackwoodseven.com>
2016-08-17 13:40:58 +0200

    go fmt

--- src/proxy/main.go
+++ src/proxy/main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "log"
-    "net/http"
-    "net/http/httputil"
-    "strings"
-    "time"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"time"
 )
 
 func Log(handler http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        before := time.Now()
-        handler.ServeHTTP(w, r)
-        log.Printf("%s %s %.4f %s\n", r.RemoteAddr, r.Method, time.Since(before), r.URL)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		before := time.Now()
+		handler.ServeHTTP(w, r)
+		log.Printf("%s %s %.4f %s\n", r.RemoteAddr, r.Method, time.Since(before), r.URL)
+	})
 }
 
 func main() {
-    port := flag.Int("port", 80, "listening on port")
-    flag.Parse()
+	port := flag.Int("port", 80, "listening on port")
+	flag.Parse()
 
-    fmt.Println("starting proxy on port", *port)
-    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), &httputil.ReverseProxy{
-        Director: func(req *http.Request) {
-            req.URL.Host = strings.Split(req.Host, ".")[0]
-            req.URL.Scheme = "http"
-        },
-    }))
+	fmt.Println("starting proxy on port", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), &httputil.ReverseProxy{
+		Director: func(req *http.Request) {
+			req.URL.Host = strings.Split(req.Host, ".")[0]
+			req.URL.Scheme = "http"
+		},
+	}))
 }
